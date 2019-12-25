861fd5b40b85be3c9b282106f7bca8f02bd1d39d
Prashant Sah <pacificprashant1228@gmail.com>
2019-12-26 00:39:05 +0545

    view modified according to design

--- MessagekitTesting/MainChatController.swift
+++ MessagekitTesting/MainChatController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MessageKit
 import InputBarAccessoryView
 
-class MainChatController: MessagesViewController, MessagesDataSource {
+class MainChatController: MessagesViewController {
     var messageList: [MockMessage] = []
 
     let refreshControl = UIRefreshControl()
@@ -51,9 +51,30 @@ class MainChatController: MessagesViewController, MessagesDataSource {
 
         messagesCollectionView.addSubview(refreshControl)
         refreshControl.addTarget(self, action: #selector(loadMoreMessages), for: .valueChanged)
+
+        changeLayoutProperties()
     }
 
-    
+    func changeLayoutProperties() {
+        if let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout {
+            layout.setMessageIncomingAvatarSize(.init(width: 45, height: 45))
+            layout.setMessageOutgoingAvatarSize(.zero)
+            layout.setMessageIncomingAvatarPosition(.init(vertical: .messageLabelTop))
+            layout.setAvatarLeadingTrailingPadding(18)
+
+            layout.setMessageIncomingMessagePadding(.init(top: 4, left: 8, bottom: 4, right: 32))
+            layout.setMessageOutgoingMessagePadding(.init(top: 4, left: 71, bottom: 4, right: 14))
+
+            layout.textMessageSizeCalculator.incomingMessageLabelInsets = .init(top: 16, left: 20, bottom: 12, right: 20)
+            layout.textMessageSizeCalculator.outgoingMessageLabelInsets = .init(top: 12, left: 20, bottom: 12, right: 20)
+
+            layout.setMessageIncomingMessageBottomLabelAlignment(.init(textAlignment: .left, textInsets: .init(top: 0, left: 71, bottom: 0, right: 0)))
+            layout.setMessageOutgoingMessageBottomLabelAlignment(.init(textAlignment: .right, textInsets: .init(top: 0, left: 0, bottom: 0, right: 32)))
+//            layout.textMessageSizeCalculator.messageLabelFont =
+
+        }
+    }
+
     func insertMessage(_ message: MockMessage) {
         messageList.append(message)
         // Reload last section to update header/footer labels and insert a new one
@@ -79,6 +100,9 @@ class MainChatController: MessagesViewController, MessagesDataSource {
 
         return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
     }
+}
+
+extension MainChatController: MessagesDataSource {
 
     func currentSender() -> SenderType {
         return SampleData.shared.currentSender
@@ -93,20 +117,18 @@ class MainChatController: MessagesViewController, MessagesDataSource {
     }
 
     func cellTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
-        if indexPath.section % 3 == 0 {
-            return NSAttributedString(string: MessageKitDateFormatter.shared.string(from: message.sentDate), attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        if isTimeLabelVisible(at: indexPath) {
+            return NSAttributedString(string: MessageKitDateFormatter.shared.string(from: message.sentDate), attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
         }
         return nil
     }
 
-    func cellBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
-
-        return NSAttributedString(string: "Read", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        return nil
     }
 
-    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
-        let name = message.sender.displayName
-        return NSAttributedString(string: name, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
+    func cellBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        return NSAttributedString(string: "Read", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
     }
 
     func messageBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
@@ -114,28 +136,49 @@ class MainChatController: MessagesViewController, MessagesDataSource {
         return NSAttributedString(string: dateString, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2)])
     }
 
+    func isTimeLabelVisible(at indexPath: IndexPath) -> Bool {
+        return indexPath.section % 3 == 0 && !isPreviousMessageSameSender(at: indexPath)
+    }
+
+    func isPreviousMessageSameSender(at indexPath: IndexPath) -> Bool {
+        guard indexPath.section - 1 >= 0 else { return false }
+        return messageList[indexPath.section].user == messageList[indexPath.section - 1].user
+    }
+
+    func isNextMessageSameSender(at indexPath: IndexPath) -> Bool {
+        guard indexPath.section + 1 < messageList.count else { return false }
+        return messageList[indexPath.section].user == messageList[indexPath.section + 1].user
+    }
+
 }
 
 extension MainChatController: MessagesLayoutDelegate {
 
     func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
-        return 18
-    }
-
-    func cellBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
-        return 17
+        if isTimeLabelVisible(at: indexPath) {
+            return 20
+        }
+        return 0
     }
 
     func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
-        return 20
+        return 0
     }
 
     func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
-        return 16
+        return 12
     }
+
+    func footerViewSize(for section: Int, in messagesCollectionView: MessagesCollectionView) -> CGSize {
+        return .init(width: messagesCollectionView.frame.width, height: 32)
+    }
+
 }
 
+
 extension MainChatController: MessagesDisplayDelegate {
+
+    // MARK: - Text Messages
 
     func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
         return isFromCurrentSender(message: message) ? .white : .purple
@@ -159,8 +202,27 @@ extension MainChatController: MessagesDisplayDelegate {
     // MARK: - All Messages
 
     func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
-        let tail: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
-        return .bubbleTail(tail, .curved)
+
+        let corners: UIRectCorner = [.topLeft, .topRight, .bottomLeft, .bottomRight]
+
+        return .custom { view in
+            let radius: CGFloat = 22.5
+            let path = UIBezierPath(roundedRect: view.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            view.layer.mask = mask
+        }
+    }
+
+    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+        avatarView.isHidden = false
+        if !isFromCurrentSender(message: message) {
+            let avatar = SampleData.shared.getAvatarFor(sender: message.sender)
+            avatarView.set(avatar: avatar)
+        } else {
+            avatarView.isHidden = true
+        }
+
     }
 
 }
